33e5836c86fc798bc452192e282704a8dfc81786
frankpolo <74099750+frankpolo@users.noreply.github.com>
2024-12-07 01:23:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST"],
     allow_headers=["*"],
 )
 
